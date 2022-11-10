ec51f38b332ece043c7f8e65e7ccbfb09d450335
Siti-ZF <113782995+Siti-ZF@users.noreply.github.com>
2022-11-10 21:25:53 +0700

    Pertemuan materi php 3

---
+++ php_pertemuan-3/Welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Halo</h2>
+    <b> Selamat Datang di website kami </b><br>
+    <i> Semoga Hari anda Menyenangkan^^ </i><br>
+    Halo! Selamat datang di Website Toko Baju yang masih merintis ini saya harap anda tidak kecewa dan datang kembali di lain hari :)
+</body>
+</html>
